d2efee8ac4dfea84b247ead50f772da61eb8a441
Dannyelgeek <daot1993@gmail.com>
2023-04-25 16:05:56 -0500

    JavaScript: Web Dynamic task 2.

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,10 +1,24 @@
-$(function() {
+$(function () {
     $('input').css('margin-right', '10px');
-    $(':checkbox')change(function () {
-        if (this.checked) {
-            alert('checked')
+    const amID = [];
+    $(':checkbox').change(function () {
+        if ($(this).prop("checked") == true) {
+            amID.push($(this).attr("data-id"));
         } else {
-            
+            amID.pop($(this).attr("data-id"));
+        }
+    });
+    const amName = [];
+    $(':checkbox').change(function () {
+        if ($(this).prop("checked") == true) {
+            amName.push($(this).attr("data-name"));
+        } else {
+            amName.pop($(this).attr("data-name"));
+        }
+        if ($.isEmptyObject(amName)) {
+            $('.amenities h4').html('&nbsp');
+        } else {
+            $('.amenities h4').text(Object.values(amName).join(', '));
         }
     });
 });
