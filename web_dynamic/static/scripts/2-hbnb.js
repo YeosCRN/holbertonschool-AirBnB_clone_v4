b0fcab12283df360945c0df853e3e545f40f0c6d
Dannyelgeek <daot1993@gmail.com>
2023-04-26 15:09:35 -0500

    JavaScript: Web Dynamic task 3.

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -21,26 +21,11 @@ $(function () {
             $('.amenities h4').text(Object.values(amName).join(', '));
         }
     });
-    $('#api_status').css({
-        'position': 'absolute',
-        'margin-top': '-55px',
-        'right': '30px',
-        'height': '40px',
-        'width': '40px',
-        'background-color': '#cccccc',
-        'rigth': '30px',
-        'margin-rigth': '30px',
-        'border-radius': '50%',
-        'display': 'inline-block'
+    $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+        if (data.status === 'OK') {
+            $('div#api_status').addClass('available');
+        } else {
+            $('div#api_status').removeClass('available');
+        }
     });
-    $.get('http://0.0.0.0:5001/api/v1/status/',
-        { status: '' }, function (data) {
-            if (data.status === 'OK') {
-                $('div #api_status').addClass('available');
-                alert('funciona');
-            } else {
-                $('div #api_status').removeClass('available');
-                alert('no funciona');
-            }
-        });
 });
